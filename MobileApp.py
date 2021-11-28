b4e896adb0b0a520b06680262d825818b959f83a
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-28 05:20:46 -0500

    Update

--- MobileApp.py
+++ MobileApp.py
@@ -4,42 +4,10 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols=1
-
-        self.inside = GridLayout()
-        self.inside.cols = 2
-               
-        self.inside.add_widget(Label(text="First Name: "))
-        self.firstname = TextInput(multiline=False)
-        self.inside.add_widget(self.firstname)
-               
-        self.inside.add_widget(Label(text="Last Name: "))
-        self.lastname = TextInput(multiline=False)
-        self.inside.add_widget(self.lastname)
-             
-        self.inside.add_widget(Label(text="Email: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    def pressed(self, instance):
-        print("Thank You")
-        first = self.firstname.text
-        last = self.lastname.text
-        email=self.email.text
-        print("First Name: ", first, "\nLast Name: ", last, "\nEmail: ", email)
-        self.firstname.text = ""
-        self.lastname.text = ""
-        self.email.text = ""
+class MyGrid(Widget):
+    pass
 
 class MyApp(App):
     def build(self):
